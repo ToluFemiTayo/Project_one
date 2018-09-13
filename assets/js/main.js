e03e5e1a9634bfa8bdb97d3389fff43fbc3ed6fa
ToluFemiTayo <omodarafemi2123@gmail.com>
2018-09-13 17:24:15 +0100

    modification on footer in the html, css and js

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,29 +22,28 @@ var _form = document.getElementsByClassName("form__fill")[0];
 var _inputs = Array.from(_form.getElementsByClassName("form-control"));
 // array.from convert html collection to arrays properties
 // console.log(_inputs);
-var keyupHandler = function(e){
-    // console.log(e.target.value);
-    // console.log(e.target.name);
-    if(e.target.name === "name" && e.target.value.length < 4){
-        this.classList.add("is-invalid");
+var keyupHandler = function (e) {
+  // console.log(e.target.value);
+  // console.log(e.target.name);
+  if (e.target.name === "name" && e.target.value.length < 4) {
+    this.classList.add("is-invalid");
+  }
+  else {
+    this.classList.remove("is-invalid");
+  };
+  if (e.target.name === "email") {
+    // console.log("email");
+    var regExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    var value = e.target.value;
+    // console.log(regExp.test(String(value).toLowerCase()));
+    if (!regExp.test(String(value).toLowerCase())) {
+      this.classList.add("is-invalid");
     }
     else {
-        this.classList.remove("is-invalid");
-    };
-    if(e.target.name === "email"){
-        // console.log("email");
-        var regExp = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        var value = e.target.value;
-        // console.log(regExp.test(String(value).toLowerCase()));
-        if (!regExp.test(String(value).toLowerCase())) {
-            this.classList.add("is-invalid");
-        }
-        else {
-            this.classList.remove("is-invalid");
-        }
+      this.classList.remove("is-invalid");
     }
-
+  }
 };
-_inputs.forEach(function(eachInputs){
-    eachInputs.addEventListener("keyup", keyupHandler);
+_inputs.forEach(function (eachInputs) {
+  eachInputs.addEventListener("keyup", keyupHandler);
 });
